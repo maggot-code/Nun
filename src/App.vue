5a8ffa9d1ce9195ad74160b6acf9d06d202fe561
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-11-13 22:34:27 +0800

    fix :-1:

--- src/App.vue
+++ src/App.vue
@@ -3,64 +3,13 @@
  * @Author: maggot-code
  * @Date: 2022-10-30 19:26:27
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-10-31 00:26:27
+ * @LastEditTime: 2022-11-13 22:31:33
  * @Description: 
 -->
-<script setup>
-import { serviceGateway } from "./server/gateway";
+<script setup></script>
 
-const gateway = serviceGateway();
-</script>
-
-<template>
-    <main class="app">
-        <section class="app-layout app-client">尝试吸吸新粉✨（按出境顺序）
-            <el-tag>应用</el-tag>
-        </section>
-        <section class="app-layout">
-            <p>物料</p>
-        </section>
-        <section class="app-layout">
-            <p>模板</p>
-        </section>
-        <section class="app-layout">
-            <p>业务</p>
-        </section>
-        <section class="app-layout">
-            <p>系统</p>
-        </section>
-        <section class="app-layout">
-            <p>编排</p>
-        </section>
-        <section class="app-layout">
-            <p>网关</p>
-        </section>
-    </main>
-</template>
+<template>123</template>
 
 <style scoped>
-.app {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    width: 100%;
-    height: 100%;
-    padding: 12px;
-    box-sizing: border-box;
-    overflow: hidden;
-}
 
-.app-layout {
-    width: 33%;
-    height: calc(33% - 6px);
-    padding: 6px;
-    border-radius: 6px;
-    border: 2px dashed #666;
-    box-sizing: border-box;
-    overflow: hidden;
-}
-
-.app-client {
-    width: 100%;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@
  * @Author: maggot-code
  * @Date: 2022-10-30 19:26:27
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-10-30 20:50:05
+ * @LastEditTime: 2022-11-13 22:31:27
  * @Description:
  */
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./style.css";
 
 const app = createApp(App);
 
