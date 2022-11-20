9b1f85c9638ebae1a46a95d6752d2c0bf0cd393d
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-11-20 19:17:56 +0800

    fix :-1:

---
+++ src/packages/internal/is.ts
@@ -0,0 +1,9 @@
+/*
+ * @FilePath: /Nun/src/packages/internal/is.ts
+ * @Author: maggot-code
+ * @Date: 2022-11-20 19:02:26
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2022-11-20 19:02:31
+ * @Description: 
+ */
+export default {}
